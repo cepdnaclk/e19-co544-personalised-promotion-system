bbeb1b4660a57fd7127c15e044f8ee6a08680a96
Sachith Dissanayaka <e19087@eng.pdn.ac.lk>
2024-06-22 11:48:43 +0530

    updated

--- test/test_preprocess.py
+++
@@ -1,9 +0,0 @@
-from scripts.preprocess import preprocess
-import os
-
-def test_preprocess():
-    preprocess()
-    assert os.path.exists('data/processed_data.csv')
-
-if __name__ == "__main__":
-    test_preprocess()
